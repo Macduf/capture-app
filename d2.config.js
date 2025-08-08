22fd662846d4cc11d9b1bbdc6dd269ec35030cf3
walter@hisp.org <walter@hisp.org>
2025-08-08 15:52:36 +0200

    customize app name, filter out program if Show in Capture App attribute value is false and bugfixes

--- d2.config.js
+++ d2.config.js
@@ -1,15 +1,15 @@
 const config = {
-    name: 'capture',
-    title: 'Capture',
-    type: 'app',
+  name: "capture-za-eidsr",
+  title: "capture-za-eidsr",
+  type: "app",
 
-    id: '92b75fd0-34cc-451c-942f-3dd0f283bcbd',
-    minDHIS2Version: '2.40',
-    coreApp: true,
+  id: "92b75fd0-34cc-451c-942f-3dd0f283bcbd",
+  minDHIS2Version: "2.40",
+  //coreApp: true,
 
-    entryPoints: {
-        app: './src/index',
-    },
+  entryPoints: {
+    app: "./src/index",
+  },
 };
 
 module.exports = config;
--- src/core_modules/capture-core/metaDataStoreLoaders/programs/queries/queryProgramsOutline.js
+++ src/core_modules/capture-core/metaDataStoreLoaders/programs/queries/queryProgramsOutline.js
@@ -1,15 +1,23 @@
 // @flow
-import { queryRecursively } from '../../IOUtils';
+import { queryRecursively } from "../../IOUtils";
+
+const showInCaptureAtrrUid = "RyYftsGAmfF";
 
 export const queryProgramsOutline = async (): Promise<Array<Object>> => {
-    const specification = {
-        resource: 'programs',
-        params: {
-            fields: 'id,version,programTrackedEntityAttributes[trackedEntityAttribute[id,optionSet[id,version]]],' +
-                'programStages[id,programStageDataElements[dataElement[id,optionSet[id,version]]]]',
-        },
-    };
+  const specification = {
+    resource: "programs",
+    params: {
+      fields:
+        "id,version,programTrackedEntityAttributes[trackedEntityAttribute[id,optionSet[id,version]]]," +
+        "programStages[id,programStageDataElements[dataElement[id,optionSet[id,version]]]]",
+      filter: [
+        `attributeValues.attribute.id:eq:${showInCaptureAtrrUid}`,
+        "attributeValues.value:eq:true",
+      ],
+    },
+  };
 
-    return (await queryRecursively(specification, { pageSize: 1000 }))
-        .flatMap(responseItem => (responseItem && responseItem.programs) || []);
+  return (await queryRecursively(specification, { pageSize: 1000 })).flatMap(
+    (responseItem) => (responseItem && responseItem.programs) || []
+  );
 };
