1e91ffd7472c96fcd3afb6baef4b1272e86abf8b
walter@hisp.org <walter@hisp.org>
2025-08-08 12:05:38 +0200

    add required dependencies & bugfix

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,31 +1,29 @@
 // @flow
 /* eslint-disable import/first */
-import './app.css';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { AppContents } from './AppContents.component';
-import {
-    RulesEngineVerboseInitializer,
-} from '../../core_modules/capture-core/components/RulesEngineVerboseInitializer';
-import {
-    MetadataAutoSelectInitializer,
-} from '../../core_modules/capture-core/components/MetadataAutoSelectInitializer';
-
+import "./app.css";
+import React from "react";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContents } from "./AppContents.component";
+import { RulesEngineVerboseInitializer } from "../../core_modules/capture-core/components/RulesEngineVerboseInitializer";
+import { MetadataAutoSelectInitializer } from "../../core_modules/capture-core/components/MetadataAutoSelectInitializer";
 
 type Props = {
-    store: ReduxStore,
+  store: ReduxStore,
 };
 
+const queryClient = new QueryClient();
+
 export const App = ({ store }: Props) => (
-    <React.Fragment>
-        <Provider
-            store={store}
-        >
-            <MetadataAutoSelectInitializer>
-                <RulesEngineVerboseInitializer>
-                    <AppContents />
-                </RulesEngineVerboseInitializer>
-            </MetadataAutoSelectInitializer>
-        </Provider>
-    </React.Fragment>
+  <React.Fragment>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <MetadataAutoSelectInitializer>
+          <RulesEngineVerboseInitializer>
+            <AppContents />
+          </RulesEngineVerboseInitializer>
+        </MetadataAutoSelectInitializer>
+      </QueryClientProvider>
+    </Provider>
+  </React.Fragment>
 );
